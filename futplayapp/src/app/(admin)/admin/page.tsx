dd9faf2851e0ccbd4a77e0b90ed44ecb71f2e940
joaquinlepeS <soluciones.flowcode@gmail.com>
2026-04-13 23:21:11 -0400

    Revert "Merge branch 'main' into Joaco"
    This reverts commit eae4b3f020cb20b5ddd355e0e3b2ef8fc90ff094, reversing
    changes made to 9d3404919d50cd409427aa67cd394f9a2f2305eb.

--- futplayapp/src/app/(admin)/admin/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function AdminPage() {
-  return <h1>Panel Admin</h1>;
-}
--- futplayapp/src/app/(dashboard)/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardPage() {
-    return (
-        <main>
-            <h1>Dashboard usuario</h1>
-        </main>
-    );
-}
