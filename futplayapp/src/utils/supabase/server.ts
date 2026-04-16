d94b36256bc77ed587cc58bb332c7fc14c080f62
joaquinlepeS <soluciones.flowcode@gmail.com>
2026-04-16 17:57:31 -0400

    Revert "logout generico"
    This reverts commit 880d448faa53aad03bfdae8af799b23271c981b8.

--- futplayapp/src/utils/supabase/server.ts
+++ futplayapp/src/utils/supabase/server.ts
@@ -1,15 +1,13 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const createClient = async () => {
-  const cookieStore = await cookies();
-  
+export const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
   return createServerClient(
-    supabaseUrl,
-    supabaseKey,
+    supabaseUrl!,
+    supabaseKey!,
     {
       cookies: {
         getAll() {
@@ -20,6 +18,8 @@ export const createClient = async () => {
             cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
           } catch {
             // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
           }
         },
       },
