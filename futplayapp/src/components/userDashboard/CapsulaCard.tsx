a1a08442d32a3e99f12d6bd1a7f2b32e5cf3a0e5
MaikHvz <soluciones.flowcode@gmail.com>
2026-04-21 05:15:59 -0400

    se agrego render de capsulas desde mock .

---
+++ futplayapp/src/components/userDashboard/CapsulaCard.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+type Props = {
+    titulo: string;
+    imagen: string;
+    coach: string;
+    categoria: string;
+    duracion: string;
+};
+
+export default function CapsulaCard({
+    titulo,
+    imagen,
+    coach,
+    categoria,
+    duracion,
+}: Props) {
+    return (
+        <div className="w-full max-w-[320px] cursor-pointer">
+
+            {/* Imagen */}
+            <div className="relative w-full h-[180px] rounded-2xl overflow-hidden group">
+
+                <img
+                    src={imagen}
+                    alt={titulo}
+                    className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
+                />
+
+                {/* Botón Play */}
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <div className="bg-white/30 backdrop-blur-md p-4 rounded-full group-hover:scale-110 transition">
+                        <div className="w-0 h-0 border-l-[10px] border-l-white border-y-[6px] border-y-transparent ml-1"></div>
+                    </div>
+                </div>
+
+                {/* Duración */}
+                <div className="absolute bottom-3 right-3 bg-black/80 text-white text-xs px-2 py-1 rounded-md">
+                    {duracion}
+                </div>
+            </div>
+
+            {/* Texto */}
+            <div className="mt-3">
+                <h3 className="text-[#00305B] font-semibold text-[16px] leading-snug">
+                    {titulo}
+                </h3>
+
+                <p className="text-gray-400 text-sm mt-1">
+                    {coach}
+                    <span className="mx-2">•</span>
+                    {categoria}
+                </p>
+            </div>
+        </div>
+    );
+}
--- futplayapp/src/components/userDashboard/Recordatorio.tsx
+++ futplayapp/src/components/userDashboard/Recordatorio.tsx
@@ -2,7 +2,7 @@
 import { LucideAlertTriangle } from "lucide-react";
 export default function Recordatorio() {
     return (
-        <div className="w-[80%] h-[70px]  rounded-xl bg-[#FFEBE8] text-[#93000A] shadow-lg border-1 border-[#F8CCCC]">
+        <div className="w-full h-[70px]  rounded-xl bg-[#FFEBE8] text-[#93000A] shadow-lg border-1 border-[#F8CCCC]">
             <div className="flex gap-3 p-4 w-full h-full items-center justify-center ">
                 <LucideAlertTriangle size={20} className="text-[#93000A]" />
                 <p className="text-[#93000A] text-[12px]"> <span className="font-semibold">Recordatorio de Normativa:</span> Las cancelaciones con menos de 4 horas de antelación generarán una penalización automática del
