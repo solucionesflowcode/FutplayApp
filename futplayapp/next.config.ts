af4115a6cceb572814445e2ea959ddef5e5f795c
MaikHvz <soluciones.flowcode@gmail.com>
2026-06-04 14:07:59 -0400

    alloweddevorigin

--- futplayapp/next.config.ts
+++ futplayapp/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["5b67724ae2feba.lhr.life"],
 };
 
 export default nextConfig;
