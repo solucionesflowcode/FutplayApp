9149d47219de1457310200b0aef5bb028cbea431
solucionesflowcode <274562159+solucionesflowcode@users.noreply.github.com>
2026-06-25 16:40:34 -0400

    sidenavbar profesor

--- futplayapp/src/app/(profesor)/profesor/layout.tsx
+++ futplayapp/src/app/(profesor)/profesor/layout.tsx
@@ -8,7 +8,7 @@ export default function ProfesorLayout({ children }: { children: React.ReactNode
     <AuthGuard allowedRoles={["profesor"]}>
       <div className="flex flex-col md:flex-row min-h-screen bg-[#f8f9fb]">
         <SidebarProfesor />
-        <main className="flex-1 overflow-y-auto">
+        <main className="flex-1 md:ml-64 overflow-y-auto">
           {children}
         </main>
       </div>
