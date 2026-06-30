e8de8afb07149b2b13fc3fca8000479d28e17321
MaikHvz <soluciones.flowcode@gmail.com>
2026-06-30 11:07:52 -0400

    Update Hero.tsx

--- futplayapp/src/components/landingPage/Hero.tsx
+++ futplayapp/src/components/landingPage/Hero.tsx
@@ -24,9 +24,9 @@ export default function Hero() {
                 <div className="hidden md:block relative">
                    <div className="absolute inset-0 bg-gradient-to-tr from-[#f59e0b] to-transparent blur-2xl opacity-30 animate-pulse pointer-events-none"></div>
                     <img
-                        src="https://images.unsplash.com/photo-1518605368461-1ee06806a6b5?q=80&w=1500&auto=format&fit=crop"
+                        src="https://images.unsplash.com/photo-1574629810360-7efbbe195018?q=80&w=1500&auto=format&fit=crop"
                         className="border-t-2 border-t-[#f59e0b] relative z-10 border-4 border-white/10 shadow-2xl object-cover h-[500px] w-full"
-                        alt="Entrenamiento de fútbol en Futplay"
+                        alt="Jóvenes entrenando en academia de fútbol"
                     />
                 </div>
             </div>
