009df2c0e6e9dede5463ed821fe81afa35c1ccc6
joaquinlepeS <soluciones.flowcode@gmail.com>
2026-07-09 17:17:40 -0400

    build correcto

--- futplayapp/next.config.ts
+++ futplayapp/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["5b67724ae2feba.lhr.life", "televisions-loaded-quote-digit.trycloudflare.com", "*.trycloudflare.com"],
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
